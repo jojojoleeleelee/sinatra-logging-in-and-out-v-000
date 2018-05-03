6e501df06b93b6e0b1173b38d721065ea3ee953b
Joanne Lee <vernapii@gmail.com>
2018-05-03 21:54:09 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,7 +4,7 @@ class Helpers
     # should it be session[:session_id] OR session[:id] ???
   end
 
-  def is_logged_in?(sessions)
-    # if
+  def is_logged_in?(session)
+    User.find_by_id(session[:user_id])
   end
 end
