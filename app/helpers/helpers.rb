00d77d1508e911f82f3cc05146de437e9604bb9b
Joanne Lee <vernapii@gmail.com>
2018-05-03 22:13:01 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 module Helpers
   class Helpers
-  def current_user(session)
-    User.find_by_id(session[:user_id])
-    # should it be session[:session_id] OR session[:id] ???
-  end
+    def current_user(session)
+      User.find_by_id(session[:user_id])
+      # should it be session[:session_id] OR session[:id] ???
+    end
 
-  def is_logged_in?(session)
-    !!current_user(session)
+    def is_logged_in?(session)
+      !!current_user(session)
+    end
   end
-end
